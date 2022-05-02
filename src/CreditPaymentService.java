ef5db15e0a0d002db65c7558502f168529b5baa8
Тарас Самойлов <kuroi22aida@gmail.com>
2022-05-02 17:44:33 +0400

    changed the formula for calculating the monthly payment

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,7 @@
 public class CreditPaymentService {
     public double calculate(int amount, double percent, int term) {
 
-        double monthlyInterestRate = percent / term / 100;
+        double monthlyInterestRate = percent / 12 / 100;
         double annuityRatio = ((monthlyInterestRate * Math.pow(1 + monthlyInterestRate, term)) / (Math.pow(1 + monthlyInterestRate, term) - 1));
         int monthlyPayment = (int) (amount * annuityRatio);
         return monthlyPayment;
